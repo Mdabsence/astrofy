93802484fc0e439c20ecb367947ded3d3f9a1bff
Matmat <145433447+Mdabsence@users.noreply.github.com>
2024-11-13 14:38:57 +0100

    Accueil finie

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astrofy | Personal Portfolio Website Template';
-export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
+export const SITE_TITLE = 'Portfolio Dabsence Mathias';
+export const SITE_DESCRIPTION = 'Portfolio de Mathias Dabsence pour l épreuve E5 du BTS SIO';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
